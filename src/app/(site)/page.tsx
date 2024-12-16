64aa5fa32151b5a6c1b18f418334f9c73eb25221
Syed Ali <syedyusufali6626@gmail.com>
2024-12-16 19:32:10 +0530

    nexcool v1

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextCommerce | Nextjs E-commerce template",
+  title: "Nexcool | Home",
   description: "This is Home for NextCommerce Template",
   // other metadata
 };
