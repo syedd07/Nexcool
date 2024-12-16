0a29ec1061673720aad81894bd25c51be4805ba5
Syed Ali <syedyusufali6626@gmail.com>
2024-12-16 20:03:10 +0530

    changed logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -63,7 +63,12 @@ const Header = () => {
           {/* <!-- header top left --> */}
           <div className="xl:w-auto flex-col sm:flex-row w-full flex sm:justify-between sm:items-center gap-5 sm:gap-10">
             <Link className="flex-shrink-0" href="/">
-              
+              <Image
+                src="/images/logo/logo_2.svg"
+                alt="Nexcool Logo"
+                width={219}
+                height={36}
+              />
             </Link>
 
             <div className="max-w-[475px] w-full">
